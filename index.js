56c21de79dd1a9b2bfc3d59b538ff6c598c7cb17
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-27 11:31:37 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -2,9 +2,37 @@ const tcp = require('bare-tcp')
 const tls = require('bare-tls')
 const http = require('bare-http1')
 
+class HTTPSSocket extends tls.Socket {
+  setKeepAlive(...args) {
+    this.socket.setKeepAlive(...args)
+
+    return this
+  }
+
+  setNoDelay(...args) {
+    this.socket.setNoDelay(...args)
+
+    return this
+  }
+
+  setTimeout(...args) {
+    this.socket.setTimeout(...args)
+
+    return this
+  }
+
+  ref() {
+    this.socket.ref()
+  }
+
+  unref() {
+    this.socket.unref()
+  }
+}
+
 exports.Agent = class HTTPSAgent extends http.Agent {
   createConnection(opts) {
-    return new tls.Socket(super.createConnection(opts), opts)
+    return new HTTPSSocket(super.createConnection(opts), opts)
   }
 
   static global = new this({ keepAlive: 1000, timeout: 5000 })
@@ -28,7 +56,7 @@ exports.Server = class HTTPSServer extends tcp.Server {
       (socket) =>
         new http.ServerConnection(
           this,
-          new tls.Socket(socket, { ...opts, isServer: true }),
+          new HTTPSSocket(socket, { ...opts, isServer: true }),
           opts
         )
     )
@@ -89,7 +117,10 @@ exports.request = function request(url, opts, onresponse) {
   }
 
   // TODO: Renable the default global agent when tests have been sorted
-  // opts.agent = opts.agent === false ? new Agent() : opts.agent || Agent.global
+  // opts.agent =
+  //   opts.agent === false
+  //     ? new exports.Agent()
+  //     : opts.agent || exports.Agent.global
 
   opts.agent = opts.agent || new exports.Agent()
 
